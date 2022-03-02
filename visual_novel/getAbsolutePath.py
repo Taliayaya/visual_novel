a254d224021647feff9877ebdab7b161084944bf
Taliayaya <ilanvinord@gmail.com>
2022-03-02 22:52:33 +0100

    Ajout de la documentation de getAbsolutePath

--- visual_novel/getAbsolutePath.py
+++ visual_novel/getAbsolutePath.py
@@ -1,5 +1,17 @@
 import os
 
 
-def getAbsolutePath(script_dir, rel_path):
+def getAbsolutePath(script_dir: str, rel_path: str) -> str:
+    """
+    Permet d'indiquer un chemin absolu dans le module à partir chemin
+    du dossier et du fichier à accéder. 
+
+    Préconditions: 
+        script_dir (str): est le chemin du fichier actuel
+        rel_path (str): est le chemin du fichier cible
+
+    Postcondition:
+        Renvoie un chemin absolu permettant d'accéder au fichier cible
+        à partir du fichier actuel
+    """
     return os.path.join(script_dir, rel_path)
