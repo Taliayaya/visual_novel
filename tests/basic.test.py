28d81b4c8293c213eaa519f741b26de914ed9b2c
Taliayaya <ilanvinord@gmail.com>
2022-03-04 21:39:43 +0100

    MAJ du test de getChoice

--- tests/basic.test.py
+++ tests/basic.test.py
@@ -1,14 +1,16 @@
+#!/usr/bin/env python3
 from visual_novel.interpreter import getChoices
 import unittest
 
 
 class BasicTests(unittest.TestCase):
-    def basicTestChoices(self):
+    def test_getChoices(self):
         testTxt = '#No~dewit.txt|...~tragedy.txt$'
-        expectOutput = [('No', 'dewit.txt'), ('...', 'tragedy.txt')]
+        expectOutput = {"type":"choice", "choice1":('No', 'dewit.txt'), "choice2": ('...', 'tragedy.txt')}
         result = getChoices(testTxt)
-        self.assertListEqual(result, expectOutput)
+        self.assertDictEqual(result, expectOutput)
 
 
 if __name__ == "__main__":
     unittest.main()
+
