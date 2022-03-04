32ebd062ccb7819fb4c1b3b110405c7f09678d6c
Taliayaya <ilanvinord@gmail.com>
2022-03-04 21:19:06 +0100

    Ajout de la documentation du GUI

--- visual_novel/gui.py
+++ visual_novel/gui.py
@@ -51,28 +51,65 @@ class App:
             self.htree = self.htree.getLeft()
         else:
             self.htree = self.htree.getRight()
+        # Supprime la zone de choix
         self.choiceContainer((None, None), (None,None), True)
+        # Récupère le nouveau fichier puis traite l'histoire
         self.currentFile = self.htree.getFile()
         self.changeFile()
         self.currentLine = 0
+        # Permet à l'utilisateur de continuer l'histoire
         self.isChoosing = False
         self.setDialogueBox(True)
 
     def setDialogueBox(self, destroy=True):
+        u"""
+        Traite l'histoire du jeu et décide quel affichage
+        chaque ligne doit recevoir en fonction de son type. 
+        
+        Récupère la variable self.history et la ligne actuelle
+        pour récupérer un dictionnaire. Ce dictionnaire contient 
+        une clef `type` qui identifie chaque ligne.
+
+        Précondition: 
+            destroy : bool 
+                indique si la frame précédente doit être
+                détruite ou non. True pour destroy, False pour keep.
+                True par défaut. 
+
+        Postcondition: 
+            Renvoie vers l'une des fonctions permettant de traiter 
+            le cas de la ligne actuelle.
+            Augmente de 1 la ligne actuelle dans l'histoire.
+        """
+
+        # Si l'utilisateur est dans un choix, empêche le refresh de
+        # la boite de dialogue pour éviter d'en créer plus d'une
         if self.isChoosing:
             return
+        # Renvoie vers l'affichage d'un texte
         if self.history[self.currentLine]['type'] != "choice":
             self.setCharacterMessage(
            self.history[self.currentLine]["name"], self.history[self.currentLine]["text"], destroy)
         else:
+            # Renvoie vers une zone de choix
             choice1 = self.history[self.currentLine]["choice1"]
             choice2 = self.history[self.currentLine]["choice2"]
             self.choiceContainer(choice1, choice2)
             self.isChoosing = True
+        # Augmente d'une ligne tant que l'historie n'est pas finie
         if self.currentLine+1 < len(self.history):
             self.currentLine += 1
 
     def chooseUsername(self):
+        u"""
+        Affiche un input permettant à l'utilisateur 
+        de choisir son username au début du jeu. 
+
+        Postcondition: 
+            Affiche l'input indiquant à l'utilisateur
+            de choisir son nom et un bouton permettant 
+            de valider l'input puis commencer le jeu.
+        """
         label = tk.Label(self.root, text="Choisi ton nom : ")
         value = tk.StringVar()
         input = tk.Entry(self.root, textvariable=string, width=50)
@@ -82,10 +119,11 @@ class App:
         bouton.grid(row=4, column=0)
 
     def menu(self):
-        """
-        Permet la création du menu
+        u"""
+        Permet la création du menu supérieur
 
-        Contient les catégories:
+        Postcondition:
+            Crée le menu qui contient les catégories:
             - FICHIER
                 - Nouvelle Partie
                 - Sauvegarder
@@ -114,7 +152,7 @@ class App:
         self.root.config(menu=self.menuContainer)
 
     def setupBackground(self):
-        """
+        u"""
         Affiche l'image choisie comme décors du jeu
         """
         img = visual_novel.getAbsolutePath.getAbsolutePath(
@@ -127,16 +165,21 @@ class App:
         canv.create_image(0, 0, anchor=tk.NW, image=self.bg)
 
     def choiceContainer(self, choice1: tuple, choice2: tuple, destroy=False):
-        """
+        u"""
         Affiche deux boutons permettant de passer dans une
         nouvelle branche de l'arbre. Demande au joueur de faire un choix
 
         Préconditions:
-            - choice1 (tuple): contient le message du bouton1 et son fichier de destination
-            - choice2 (tuple): contient le message du bouton2 et son fichier de destination
+            choice1 : tuple
+                contient le message du bouton1
+                et son fichier de destination
+            choice2 : tuple 
+                contient le message du bouton2 
+                et son fichier de destination
 
         Postcondition:
-            Affiche deux boutons de message tuple[0] et de destination tuple[1]
+            Affiche deux boutons de message tuple[0]
+            et de destination tuple[1]
         """
         if destroy:
             self.choiceFrame.destroy()
@@ -155,15 +198,22 @@ class App:
         print(self.choiceFrame)
 
     def setCharacterMessage(self, name: str, message: str, destroy: bool=True):
-        """Affiche le message du personnage sur l'interface et retire le précédent
+        u"""
+        Affiche le message du personnage sur l'interface
+        et retire le précédent
 
         Préconditions:
-            - name (str) est le nom du personnage qui parle
-            - message (str) est le message associé au personnage
-            - destroy (bool) retire ou non le message précédent
+            name : str
+                est le nom du personnage qui parle
+            message : str
+                est le message associé au personnage
+            destroy : bool
+                retire ou non le message précédent
 
         Postcondition:
-            Affiche le message name/message sur l'interface"""
+            Affiche le message name/message sur l'interface
+        """
+
         if destroy:
             self.char.destroy()
         self.char = tk.LabelFrame(self.root, text=name, padx=20, pady=20)
