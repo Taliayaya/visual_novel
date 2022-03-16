e302e8177dcadc0268f8a7a3b8d49405b7129849
Taliayaya <ilanvinord@gmail.com>
2022-03-16 08:48:58 +0100

    Ajout de nouveaux noeuds dans l'histoire

--- visual_novel/history_tree.py
+++ visual_novel/history_tree.py
@@ -1,10 +1,17 @@
 import visual_novel.arbre as abr
 
+
 def getHistoryTree():
     arbre = abr.Arbre('init', 'init.txt')
     arbre.addLeft('L1', 'L1.txt')
     arbre.addRight('R1', 'R1.txt')
-    print(arbre)
+    L1 = arbre.getLeft()
+    L1.addLeft('LL2', 'LL2.txt')
+    R1 = arbre.getRight()
+    R1.addLeft('LR2', 'LR2.txt')
     return arbre
+
+
 if __name__ == "__main__":
-    getHistoryTree()
+    print(
+        getHistoryTree())
