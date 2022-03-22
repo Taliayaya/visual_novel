b95a843fa6ee95b1c2d075addc9fa8cc70c02cca
Taliayaya <ilanvinord@gmail.com>
2022-03-22 14:56:35 +0100

    Ajout du test de la fonction getHistory

--- tests/basic.test.py
+++ tests/basic.test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from visual_novel.interpreter import getChoices, getDialogue, getBackground, getDescription
+from visual_novel.interpreter import getChoices, getDialogue, getBackground, getDescription, getHistory
 import unittest
 
 
@@ -12,7 +12,8 @@ class BasicTests(unittest.TestCase):
         """
 
         testTxt = '#No~dewit.txt|...~tragedy.txt$'
-        expectOutput = {"type":"choice", "choice1":('No', 'dewit.txt'), "choice2": ('...', 'tragedy.txt')}
+        expectOutput = {"type": "choice", "choice1": (
+            'No', 'dewit.txt'), "choice2": ('...', 'tragedy.txt')}
         result = getChoices(testTxt)
         self.assertDictEqual(result, expectOutput)
 
@@ -23,10 +24,11 @@ class BasicTests(unittest.TestCase):
         """
 
         testTxt = "-Obiwan Kenobi@: -Hello there"
-        expectOutput = {"type":"dialogue", "name": "Obiwan Kenobi", "image": '', "text":"Hello there"}
+        expectOutput = {"type": "dialogue", "name": "Obiwan Kenobi",
+                        "image": '', "text": "Hello there"}
         result = getDialogue(testTxt)
         self.assertDictEqual(result, expectOutput)
-    
+
     def test_getDialogue_with_chr_img(self):
         u"""Test la fonction getDialogue en ayant inclue une image pour le personnage 
         ainsi que sa position (left ou right) sur le GUI
@@ -34,7 +36,8 @@ class BasicTests(unittest.TestCase):
         Le résultat attendu est un dictionnaire utilisable par l'interpréteur"""
 
         testTxt = "-Obiwan Kenobi@truejesus.png,l: -Hello there !"
-        expectOutput = {"type":"dialogue", "name": "Obiwan Kenobi", "image":["truejesus.png", "l"], "text":"Hello there !"}
+        expectOutput = {"type": "dialogue", "name": "Obiwan Kenobi", "image": [
+            "truejesus.png", "l"], "text": "Hello there !"}
         result = getDialogue(testTxt)
         self.assertDictEqual(result, expectOutput)
 
@@ -44,8 +47,8 @@ class BasicTests(unittest.TestCase):
         Le message de base est une ligne type pouvant être mise dans le script 
         Le résultat attendu est un dictionnaire utilisable par l'interpréteur"""
 
-        testTxt = '=space.jpg\n' # Le retour à la ligne est nécéssaire
-        expectOutput = {"type":"bg", "name":"space.jpg"}
+        testTxt = '=space.jpg\n'  # Le retour à la ligne est nécéssaire
+        expectOutput = {"type": "bg", "name": "space.jpg"}
         result = getBackground(testTxt)
         self.assertDictEqual(result, expectOutput)
 
@@ -56,11 +59,31 @@ class BasicTests(unittest.TestCase):
         Le résultat attendu est un dictionnaire utilisable par l'interpréteur"""
 
         testTxt = "I use Arch btw"
-        expectOutput = {"type":"description", "name":'', "text":"I use Arch btw"}
+        expectOutput = {"type": "description",
+                        "name": '', "text": "I use Arch btw"}
         result = getDescription(testTxt)
         self.assertDictEqual(result, expectOutput)
+
+    def test_getHistory(self):
+        file = 'test.txt'
+        result = getHistory(file)
+        expectOutput = [
+            {'type': 'inf_sound', 'name': 'duelofthefate.wav'},
+            {'type': 'bg', 'name': 'fight.jpg'},
+            {'type': 'description', 'name': '', 'text': 'Hello There !\n'},
+            {'type': 'sound', 'name': 'lightsaber.wav'},
+            {'type': 'dialogue', 'name': 'General Grievous',
+             'text': ' "General Kenobi !\n', 'image': ''},
+            {'type': 'dialogue', 'name': 'General Grievous',
+             'text': ' "General Kenobi ! You\'re a bold one !"', 'image': ''},
+            {'type': 'dialogue', 'name': 'Obiwan Kenobi',
+             'text': ' "It\'s over Grievous, I\'ve the highgro\n',
+             'image': ['obi.png', 'l']},
+            {'type': 'stop_inf_sound', 'num': 0},
+            {'type': 'choice', 'choice1': ('No', 'dewit.txt'),
+             'choice2': ('...', 'tragedy.txt')}]
+        self.assertListEqual(result, expectOutput)
 
 
 if __name__ == "__main__":
     unittest.main()
-
